bcc65e3b4e105da08ecae139530e390494c8f60c
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2021-02-18 22:13:04 -0300

    Contruido novo index.js e index.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
+
+function Board(){
+    return (
+        <div>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
+            <div className="board-row">
+                <button className="square">X</button>
+                <button className="square">X</button>
+                <button className="square">X</button>
+            </div>
+        </div>
+    );
+}
+
+//Criação do componente Game
+function Game(){
+    return (
+        <div className='game'>
+            <div className='game-board'>
+                <Board/>
+            </div>
+            <div className='game-info'>
+                Info
+            </div>
+        </div>
+    );
+}
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    //Elemento que será renderizado
+    <Game/>,
+    //Local em que esse elemento seá renderizado
+    document.getElementById('root')
 );
